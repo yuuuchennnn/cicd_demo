bff6feaec489c17a9b7d19274d5c2966d7f094ab
yuchen.wang <yuchen.wang@kiwigame.com>
2026-03-07 11:50:04 +0800

    save

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,11 +1,13 @@
 import pytest
 from app import create_app
 
+
 @pytest.fixture
 def client():
     app = create_app()
     app.testing = True
     return app.test_client()
+
 
 def test_ping(client):
     response = client.get("/ping")
